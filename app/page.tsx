5a2f7c4f8d1f7da5863227c9899a1634496b0ab1
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-11-16 23:29:09 +0700

    add video success!!

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,19 @@
 'use client'; // This marks the file as a client component
 
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api';
 import { motion } from 'framer-motion';
-// import Image from 'next/image';
-// import accident from '@/public/';
 import { Disclosure } from '@headlessui/react'
-// import ProgressCard from './components/ProgressCard';
-// import CardList from './components/AccidentList';
 import InfoTable from './components/InfoTable';
+
 const center = { lat: 13.73113567541045, lng: 100.78116724040248 };
 
 const Home = () => {
-  // console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY); // Check if it prints the API key
-  // const [isControlsVisible, setIsControlsVisible] = useState(false);
-  // const toggleControls = () => setIsControlsVisible(!isControlsVisible);
 
   const [isVisible, setIsVisible] = useState(false);
   const handleClick = () => setIsVisible(!isVisible);
 
-  // const [isOpen, setIsOpen] = useState(false);NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
     libraries: ['places'],
@@ -47,7 +40,6 @@ const Home = () => {
       alert('กรุณากรอกที่เกิดเหตุ');
       return;
     }
-    // console.log(destinationRef)
     const directionsService = new google.maps.DirectionsService();
     try {
       const results = await directionsService.route({
@@ -55,7 +47,6 @@ const Home = () => {
         destination: destinationRef.current.value,
         travelMode: google.maps.TravelMode.DRIVING,
       });
-      // console.log(results);
   
       if (
         results.routes &&
@@ -80,37 +71,7 @@ const Home = () => {
     }
   };
   
-  // const checkRoute = async () => {
-  //   const directionsService = new google.maps.DirectionsService();
-  //   try {
-  //     const results = await directionsService.route({
-  //       origin: originRef.current.value,
-  //       destination: destinationRef.current.value,
-  //       travelMode: google.maps.TravelMode.DRIVING,
-  //     });
-  
-  //     if (
-  //       results.routes &&
-  //       results.routes.length > 0 &&
-  //       results.routes[0].legs &&
-  //       results.routes[0].legs.length > 0
-  //     ) {
-  //       const leg = results.routes[0].legs[0];
-  
-  //       setDirectionsResponse(results);
-  //       setDistance(leg.distance ? leg.distance.text : 'Distance not available');
-  //       setDuration(leg.duration ? leg.duration.text : 'Duration not available');
-  //     } else {
-  //       console.error('No route found');
-  //       setDistance('No route found');
-  //       setDuration('No route found');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error calculating route:', error);
-  //     setDistance('Error calculating route');
-  //     setDuration('Error calculating route');
-  //   }
-  // };
+
 
   const clearRoute = () => {
     setDirectionsResponse(null);
@@ -274,37 +235,28 @@ const Home = () => {
           exit={{ opacity: 0 }}
         >
           <motion.div
-            className="bg-white p-4 rounded-lg shadow-lg w-100 lg:p-6 w-100"
+            className="bg-white p-4 mx-2 my-2 rounded-lg shadow-lg w-100 lg:p-6 w-100"
             initial={{ scale: 0.5 }}
             animate={{ scale: 1 }}
             exit={{ scale: 0.5 }}
             transition={{ duration: 0.3 }}
           >
             <h2 className="text-3xl font-semibold text-center text-red-500 lg:text-5xl">‼️ เกิดอุบัติเหตุ ‼️</h2>
-            {/* <Image src={accident} alt="Warning Icon" className='mx-auto w-80 mt-4 rounded rounded-xl' /> */}
-            {/* <video
-              controls
+            <video
               width="640"
               height="360"
               className="rounded-lg shadow-lg mt-4"
+              loop
+              muted
+              autoPlay
             >
-              <source src="/public/accident2.mp4" type="video/mp4" />
-            </video> */}
-            <iframe
-              width="640"
-              height="360"
-              src={`https://www.youtube.com/embed/ZQ9Haw3maiM?autoplay=1&modestbranding=1&controls=0&showinfo=0&rel=0&fs=0`}
-              title="YouTube video player"
-              className='rounded-lg shadow-lg mt-4'
-              frameBorder="0"
-              allow="autoplay; encrypted-media;"
-              allowFullScreen
-            />
+              <source src="/accident2.mp4" type="video/mp4" />
+            </video>
 
 
               <div className="mt-6">
                 <p className="text-center text-black"><span className='font-bold'>ประเภทอุบัติเหตุ :</span> อุบัติเหตุหนัก</p>
-                <p className="text-center mt-2 text-black"><span className='font-bold'>เหตุไฟไหม้ :</span> ❌</p>
+                <p className="text-center mt-2 text-black"><span className='font-b old'>เหตุไฟไหม้ :</span> ❌</p>
                 <p className="text-center mt-2 text-black"><span className='font-bold'>สถานที่เกิดเหตุ :</span> แยกสวนสยาม ถนนเสรีไทย เขตคันนายาว</p>
                 <p className="text-center mt-2 text-black font-bold">เวลา : <span className='underline'>17:13 P.M.</span> </p>
               </div>
